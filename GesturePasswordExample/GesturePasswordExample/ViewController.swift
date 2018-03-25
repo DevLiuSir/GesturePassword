1b8e049f6e3010433390e6da0ef3bf7b85060777
ChinaHackers <93428739@qq.com>
2018-03-25 19:40:47 +0800

    init project.

---
+++ GesturePasswordExample/GesturePasswordExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GesturePasswordExample
+//
+//  Created by Liu Chuan on 2018/3/25.
+//  Copyright © 2018年 LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
